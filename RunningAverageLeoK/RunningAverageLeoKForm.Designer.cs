fff79eb26481091d30e8061ae51ae82e7fe6d95b
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-11 12:59:06 -0400

    Initial commit

---
+++ RunningAverageLeoK/RunningAverageLeoKForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RunningAverageLeoK
+{
+    partial class frmRunningAverageLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(12, 27);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(187, 16);
+            this.lblRequest.TabIndex = 0;
+            this.lblRequest.Text = "Enter a number from 0-100";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(205, 26);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClose.Location = new System.Drawing.Point(12, 54);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(171, 13);
+            this.lblClose.TabIndex = 2;
+            this.lblClose.Text = "(Enter -1 to end the program)";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(43, 109);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(99, 28);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(40, 204);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(73, 18);
+            this.lblAverage.TabIndex = 4;
+            this.lblAverage.Text = "Average:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(119, 204);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 18);
+            this.lblAnswer.TabIndex = 5;
+            // 
+            // frmRunningAverageLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 261);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblRequest);
+            this.Name = "frmRunningAverageLeoK";
+            this.Text = "Running Average by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequest;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ RunningAverageLeoK/RunningAverageLeoKForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningAverageLeoK
+{
+    public partial class frmRunningAverageLeoK : Form
+    {
+        //variables
+        int counter = 0;
+        double total = 0;
+        double userNum;
+
+        public frmRunningAverageLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //incriment the counter
+            counter++;
+            userNum = Convert.ToDouble(txtInput.Text);
+
+            //if the user enters a number that is 0-100 add it to the total
+            if (userNum >= 0)
+            {
+                total = total + userNum;
+            }
+            else if (userNum == -1)
+            {
+                MessageBox.Show("Running Average Ended", "Running Average");
+                this.btnCalculate.Enabled = false;
+                this.txtInput.Visible = false;
+            }
+
+            //diplay the average
+            lblAnswer.Text = Convert.ToString(total / counter);
+        }
+    }
+}
